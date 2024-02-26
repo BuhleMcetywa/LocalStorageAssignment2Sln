c49e0614bdd55ff57c136a634b06d0990655e884
BuhleMcetywa <buhlemcetywa07@gmail.com>
2024-02-26 10:50:27 +0200

    First commit

--- LocalStorageAssignment2/Models/Profile.cs
+++ LocalStorageAssignment2/Models/Profile.cs
@@ -6,14 +6,87 @@ using System.Threading.Tasks;
 
 namespace LocalStorageAssignment2.Models
 {
-    internal class Profile
+    using System.ComponentModel;
+
+    public class Profile : INotifyPropertyChanged
     {
+        private string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
 
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string EmailAddress { get; set; }
-        public string Bio { get; set; }
-        public string PicturePath { get; internal set; }
+        private string _surname;
+        public string Surname
+        {
+            get { return _surname; }
+            set
+            {
+                if (_surname != value)
+                {
+                    _surname = value;
+                    OnPropertyChanged(nameof(Surname));
+                }
+            }
+        }
 
+        private string _emailAddress;
+        public string EmailAddress
+        {
+            get { return _emailAddress; }
+            set
+            {
+                if (_emailAddress != value)
+                {
+                    _emailAddress = value;
+                    OnPropertyChanged(nameof(EmailAddress));
+                }
+            }
+        }
+
+        private string _bio;
+        public string Bio
+        {
+            get { return _bio; }
+            set
+            {
+                if (_bio != value)
+                {
+                    _bio = value;
+                    OnPropertyChanged(nameof(Bio));
+                }
+            }
+        }
+
+        private string _picturePath;
+        public string PicturePath
+        {
+            get { return _picturePath; }
+            set
+            {
+                if (_picturePath != value)
+                {
+                    _picturePath = value;
+                    OnPropertyChanged(nameof(PicturePath));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
+
 }
+
